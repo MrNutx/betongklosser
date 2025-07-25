7d75b57820ebdd97dafff596f1487aaa56660860
MrNutx <knuts2308@gmail.com>
2025-07-25 19:21:45 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,13 +40,13 @@
 			offset: function() { return $nav.height() - 5; }
 		});
 
-	// Nav. $('#logo').html()
+	// Nav. 
 
 		// Title Bar.
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + Betongklosser.com + '</span>' +
+					'<span class="title">' + $('#logo').html() + '</span>' +
 				'</div>'
 			)
 				.appendTo($body);
